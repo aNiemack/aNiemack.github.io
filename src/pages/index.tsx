2a0b947afc862561f927bc058a9417e966c5292a
Andrew Niemack <andrew@Andrews-MacBook-Air.local>
2023-07-29 19:32:21 -0600

    Update titles.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -454,4 +454,4 @@ const IndexPage: React.FC<PageProps> = () => {
 
 export default IndexPage;
 
-export const Head: HeadFC = () => <title>Home Page</title>;
+export const Head: HeadFC = () => <title>NightHawk Systems</title>;
